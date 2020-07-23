f2315420e8e016d4e629a45efdf4bb7067ff4816
h.kamiyo <h.kamiyo@april-knights.com>
2020-07-23 11:35:32 +0900

    updaate 実行時間修正、タイムゾーン修正確認用。部屋も放牧部屋に変更

--- slack_bot_func.py
+++ slack_bot_func.py
@@ -15,13 +15,13 @@ def send_message(channel, message):
     client.chat_postMessage(channel=channel, text=message)
 
 # 本番用設定値 毎月12時00分00秒にtime_jobを実行
-@sched.scheduled_job('cron', day=1, hour=12, minute=10, second=0)
-# @sched.scheduled_job('cron', day=27, hour=16, minute=42, second=0)
+# @sched.scheduled_job('cron', day=1, hour=12, minute=0, second=0)
+@sched.scheduled_job('cron', day=23, hour=17, minute=0, second=0)
 def timed_job():
     message = reaction_sum.get_message()
     print(message)
-    send_message('CA798CMV0', message)
-
+    # send_message('CA798CMV0', message)
+    send_message('G0149FE9SAW', message)
 
 # python slack_bot_func.py　で呼んだ時に実行される
 if __name__ == "__main__":
