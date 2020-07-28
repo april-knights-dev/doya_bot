b86c506085de23db526b4f2849809a6974767b53
h.kamiyo <h.kamiyo@april-knights.com>
2020-07-28 22:57:07 +0900

    fix 調整

--- run.py
+++ run.py
@@ -1,6 +1,26 @@
 # coding: utf-8
+import os
+from threading import Thread
 
+import slack
 from slackbot.bot import Bot
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from plugins.reaction_sum import get_message
+
+
+client = slack.WebClient(token=os.environ["WEBAPI_TOKEN"])
+sched = BlockingScheduler()
+
+
+def send_message(channel, message):
+    client.chat_postMessage(channel=channel, text=message)
+
+
+@sched.scheduled_job("cron", day=1, hour=12, minute=0, second=0)
+def timed_job():
+    message = get_message()
+    send_message("G0149FE9SAW", message)
 
 
 def main():
@@ -10,4 +30,11 @@ def main():
 
 if __name__ == "__main__":
     print("start slackbot")
-    main()
+
+    # RTMbotの起動
+    job = Thread(target=main)
+    job.start()
+
+    # APSchedulerの起動
+    job = Thread(target=sched.start)
+    job.start()
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,10 +2,10 @@
 import os
 
 # botアカウントのトークンを指定
-API_TOKEN = os.environ["API_TOKEN"]
+API_TOKEN = os.environ["BOTAPI_TOKEN"]
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "すみません、それはまだわかりません"
 
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
-PLUGINS = ['plugins']
+PLUGINS = ["plugins"]
